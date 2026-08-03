8790fb744633dd2fc5cf5a896b73909446574432
Username4D <studio@kremzow.org>
2026-08-03 21:48:24 +0200

    added level base scene and finish scene, added player finish person

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,13 +5,17 @@ extends CharacterBody2D
 @export var acceleration = 180
 @export var friction = 300
 @export var speed = 0
+@export var max_finish_speed = 1
+@export var finish_acceleration = 2
+@export var finish_speed = 0
 
 enum states {ALIVE, FINISHED, DEAD}
 @export var state = states.ALIVE
 
 @export var spawn_point = Vector2.ZERO
-
+var finish_position = Vector2.ZERO
 signal death
+signal finished
 
 func _physics_process(delta: float) -> void:
 	if state == states.ALIVE:
@@ -29,11 +33,23 @@ func _physics_process(delta: float) -> void:
 		velocity.x = speed
 		velocity.y += get_gravity().y * delta
 		move_and_slide()
-
+	if state == states.FINISHED:
+		finish_speed = move_toward(finish_speed, max_finish_speed, delta * finish_acceleration)
+		self.position = self.position.move_toward(finish_position, finish_speed)
+		self.scale = clamp(self.scale.move_toward(Vector2(self.position.distance_to(finish_position) / 64, self.position.distance_to(finish_position) / 64), delta), Vector2(0, 0), Vector2(1, 1))
+		self.modulate.a = clamp(move_toward(self.modulate.a, self.position.distance_to(finish_position) / 64, delta), 0, 1)
+		self.rotation_degrees += delta * finish_speed * 60
 func kill():
-	state = states.DEAD
-	$animations.play("death")
-	await get_tree().create_timer(0.7).timeout
-	self.position = spawn_point
-	$animations.play("RESET")
-	state = states.ALIVE
+	if state == states.ALIVE:
+		state = states.DEAD
+		$animations.play("death")
+		await get_tree().create_timer(0.7).timeout
+		self.position = spawn_point
+		$animations.play("RESET")
+		state = states.ALIVE
+
+func finish(finish_object: Node):
+	finished.emit()
+	finish_position = finish_object.position
+	state = states.FINISHED
+	speed = 0
