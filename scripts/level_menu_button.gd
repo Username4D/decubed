77fb0fc7d5fe220c6eaab74fca819cf823c3223a
Username4D <studio@kremzow.org>
2026-08-06 21:50:05 +0200

    added functionality for level_menu_button to launch level scene

--- scripts/level_menu_button.gd
+++ scripts/level_menu_button.gd
@@ -3,10 +3,13 @@ extends Panel
 @export var next_button_line_position = Vector2.ZERO
 @export var id = 0
 
+signal level_button_pressed(node: Node)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$next_button_line.add_point(next_button_line_position * 2)
 	$Sprite2D.rotation_degrees = 90 * randi_range(0, 3)
+	$button.pressed.connect(func(): level_button_pressed.emit(self))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/level_selection_menu_page.gd
+++ scripts/level_selection_menu_page.gd
@@ -6,6 +6,10 @@ extends Node2D
 func _ready() -> void:
 	for i in $level_buttons.get_children():
 		i.id = int(i.name) + offset
+		i.level_button_pressed.connect(launch_level)
+func launch_level(button_node: Node):
+	get_tree().change_scene_to_file("res://scenes/levels/%d.tscn" % button_node.id)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
