71d6812929dd48322dd56e9986aa93a2c632e57b
Username4D <studio@kremzow.org>
2026-08-08 23:08:52 +0200

    added magnet object and new level utilizing it, more coming soon

---
+++ scripts/magnet.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var is_touching_player = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in range(0, 72.0):
+		$line.add_point(Vector2(cos(i / 72.0 * 2 * PI), sin(i / 72.0 * 2 * PI)) * 256)
+		print(Vector2(cos(i / 18.0 * 2 * PI), sin(i / 18.0 * 2 * PI)) * 256)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	$particles.self_modulate.a = move_toward($particles.self_modulate.a, 1 if Input.is_action_pressed("ui_up") and is_touching_player else 0, delta)
+	
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		is_touching_player = true
+		
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		is_touching_player = false
