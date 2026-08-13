87b4ec2b3f6ad94019ca17771243a41afd06ca1b
Username4D <studio@kremzow.org>
2026-08-13 23:43:28 +0200

    added 10 new levels using new saw/sawline object, including deco, palettes, formating etc

--- scripts/saw_line.gd
+++ scripts/saw_line.gd
@@ -1,11 +1,13 @@
 extends Line2D
 
-var current_saw_point = 1
+@export var current_saw_point = 1
 var saw_speed = 200
+@export var saw_scale = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$saw.position = self.points[0]
-
+	self.self_modulate.a = 0
+	$saw.position = self.points[current_saw_point - 1]
+	$saw.scale = Vector2(saw_scale, saw_scale)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
