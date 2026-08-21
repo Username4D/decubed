2675f45ddf95d4ae75565a98391c6f41a452a617
Username4D <studio@kremzow.org>
2026-08-21 23:13:36 +0200

    fully moved away from instance uniform based color palette shaders

--- scripts/magnet.gd
+++ scripts/magnet.gd
@@ -7,6 +7,8 @@ func _ready() -> void:
 		$line.add_point(Vector2(cos(i / 72.0 * 2 * PI), sin(i / 72.0 * 2 * PI)) * 256)
 		print(Vector2(cos(i / 18.0 * 2 * PI), sin(i / 18.0 * 2 * PI)) * 256)
 	$particles.visible = SettingsHandler.settings["particles_enabled"]
+	await get_tree().process_frame
+	self.modulate = ColorPalettes.current_palette.dark
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
